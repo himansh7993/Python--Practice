08ff17c343836d494829378db9b36d15d108ba04
himansh7993 <147494573+himansh7993@users.noreply.github.com>
2026-06-24 12:54:30 +0530

    Implement basic number guessing game

---
+++ number_guessing_game.py
@@ -0,0 +1,7 @@
+import random
+numbern= random.randint(1,10)
+guess =int(input ("Guess a number between 1 and 10:"))
+if guess == number :
+  print("congratulations! you guessed correctly.")
+  else
+  print("wrong guess. The number was",number)
